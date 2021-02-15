99e4f5cc54af5da32de381a4d70e7dac8152ca54
JavideSs <javimelladoo@gmail.com>
2021-02-15 18:54:37 +0100

    Add comentarios en clase HadaP1

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Esta clase contiene metodos para convertir
+    /// de segundos a minutos y de minutos a segundos
+    /// </summary>
     class HadaP1
     {
+        /// <summary>
+        /// Convierte de segundos a minutos
+        /// </summary>
+        /// <param name="s">segundos</param>
+        /// <returns>minutos</returns>
         public static double Seconds2Minutes(double s)
         {
             if (s == 0)
@@ -16,6 +25,11 @@ namespace hada_p1
             return s / 60;
         }
 
+        /// <summary>
+        /// Convierte de minutos a segundos
+        /// </summary>
+        /// <param name="m">minutos</param>
+        /// <returns>segundos</returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
